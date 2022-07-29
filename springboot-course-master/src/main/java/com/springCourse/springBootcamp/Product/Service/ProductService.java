dfd16472e90e33eb8fa371a387daf3eac887c106
Ege Demirbas <30300963+Egerion@users.noreply.github.com>
2022-07-29 20:35:59 +0300

    homeworks and course content added

---
+++ springboot-course-master/src/main/java/com/springCourse/springBootcamp/Product/Service/ProductService.java
@@ -0,0 +1,45 @@
+package com.springCourse.springBootcamp.Product.Service;
+
+import com.springCourse.springBootcamp.Product.Dao.ProductDao;
+import com.springCourse.springBootcamp.Product.Entity.Product;
+import com.springCourse.springBootcamp.Seller.Entity.Seller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductDao productDao;
+
+    public List<Product> findAll() {
+        return productDao.findAll();
+    }
+
+    public Product findAllById(Long Id) {
+        return productDao.findAllById(Id);
+    }
+
+    public Product findAllByTitle(String title) {
+        return productDao.findAllByTitle(title);
+    }
+
+    public List<Product> findAllBySeller(Seller seller) {
+        return findAllBySeller(seller);
+    }
+
+    public Product findAllByIsDeleted(boolean isDeleted) {
+        return productDao.findAllByIsDeleted(isDeleted);
+    }
+
+    public Product createNewProduct(Product product) {
+        return productDao.save(product);
+    }
+
+    public void deleteGivenProduct(Product product) {
+        productDao.delete(product);
+    }
+
+}
